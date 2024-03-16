820f81ddd54cfa01abc3a75fd521ae01915c2d67
Meiroudii <119019700+Meiroudii@users.noreply.github.com>
2024-03-16 21:49:40 +0800

    add about and contact page

--- main.py
+++ main.py
@@ -5,15 +5,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    home = "Home"
+    return render_template("index.html",
+                           home=home)
 
 @app.route("/about")
 def about_page():
-    return render_template("about.html")
+    about = "About"
+    return render_template("about.html",
+                           about=about)
 
 @app.route("/contact")
 def contact_page():
-    return render_template("contact.html")
+    contact = "Contact"
+    return render_template("contact.html",
+                           contact=contact)
 
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=8000)
