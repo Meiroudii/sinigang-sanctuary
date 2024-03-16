0750b92c51b69ec06e9619cbc8fe3bfbecb20f7a
Meiroudii <119019700+Meiroudii@users.noreply.github.com>
2024-03-16 11:14:39 +0800

    add jinja2 templating and tailwindcss

--- main.py
+++ main.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/about")
+def about_page():
+    return render_template("about.html")
+
+@app.route("/contact")
+def about_page():
+    return render_template("contact.html")
+
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=8000)
