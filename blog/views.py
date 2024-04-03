984778a6ff228bca4f4250e2d5e4c49acb52ae1a
Meiroudii <119019700+Meiroudii@users.noreply.github.com>
2024-04-03 22:07:55 +0800

    test: html rendering with django

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, "blog/base.html")
+    return render(request, "blog/index.html")
+
+def about(request):
+    return render(request, "blog/about.html")
